da70a45857feaf7c14e0257012c63c6d52d69b4e
Dorka Tamas <dorottya.tamas@ge.com>
2017-03-05 23:05:48 +0100

    log updated for app.listen

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 var express = require('express')
 var app = express();
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/helloWorld', function (req, res) {
-  res.send('<h1>Hello World!</h1>')
+    res.send('<h1>Hello World!</h1>')
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+    console.log('App1 listening on port 3000!')
 });
