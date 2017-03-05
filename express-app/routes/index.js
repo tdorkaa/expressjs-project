392b6bd68f124f8f2574d6d74af6ef74fbb8f192
Dorka Tamas <dorottya.tamas@ge.com>
2017-03-05 22:18:06 +0100

    2nd application

--- express-app/routes/index.js
+++ express-app/routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/test/:name', function(req, res, next) {
+  res.render('test', {output: req.params.name});
+});
+
+router.post('/test/submit', function(req, res, next) {
+  var name = req.body.name;
+  res.redirect('/test/' + name);
+});
+
 module.exports = router;
